a125362c2ca0c4d49099cc0d3fd162718efe2740
Mrfail2 <35237265+Mrfail2@users.noreply.github.com>
2018-01-08 15:31:13 -0500

    Update Robot.java

--- 2018-PowerUp/src/org/frc1721/powerup/Robot.java
+++ 2018-PowerUp/src/org/frc1721/powerup/Robot.java
@@ -29,7 +29,6 @@ public class Robot extends IterativeRobot {
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
-	 * 
 	 */
 	@Override
 	public void robotInit() {
